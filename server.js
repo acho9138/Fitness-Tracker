4c3fbce3134528132369959274c75551669dcfab
Anna C <anna.s.chong@gmail.com>
2020-09-15 09:58:44 +1000

    ADD server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Routes
+app.use(require("./routes/apiRoutes.js"));
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
