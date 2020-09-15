b0346050cd4a9d70a466b8b6843ededb449516ee
Anna C <anna.s.chong@gmail.com>
2020-09-15 10:53:48 +1000

    Deploying

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/WorkoutDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 // Routes
 app.use(require("./routes/apiRoutes.js"));
